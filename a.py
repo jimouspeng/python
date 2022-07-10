d79f26496e9d8578c5e60d73a2b6f5595a848557
jimous <jimouspeng@xiaoe-tech.com>
2022-07-10 21:20:28 +0800

    fix，init

---
+++ a.py
@@ -0,0 +1,19 @@
+'''
+Date: 2022-07-09 08:44:20
+LastEditors: jimouspeng
+Description: 描述文件内容
+LastEditTime: 2022-07-09 08:59:21
+FilePath: \python\a.py
+
+'''
+
+# 单行注释
+
+print("Hello, World!")
+
+if True:
+    print('true')
+else:
+    print('false')
+str = 'hahahah'
+print(str)
